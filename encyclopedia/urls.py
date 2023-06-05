7d2700e34329716508c7c72d4dc411d32a6be7a8
Fikremariam Besrat <118328728+fonkamariam@users.noreply.github.com>
2023-06-05 13:50:36 +0300

    Worked on Creating a new page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("New_Page", views.cnp, name="cnp"),
+    path("Random_Page", views.rp, name="rp")
 ]
+
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from . import util
 
 
@@ -8,3 +9,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def cnp(request):
+    if request.method =='POST':
+        title= request.POST["filename"]
+        body=request.POST["body"]
+        for name in util.list_entries():
+            if name==title:
+                return render(request,"encyclopedia/cnp.html",{"body":body,"message":f"Username '{title}' Taken...Try Again"})
+        util.save_entry(title,body)
+        return HttpResponseRedirect(reverse("index"))
+    return render(request, "encyclopedia/cnp.html",{"names":util.list_entries})
+
+def rp(request):
+    pass
